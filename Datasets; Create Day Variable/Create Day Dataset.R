2c4adc4c680970f89e701598bc78c22a4f95747d
Jiaqi Bi <garybi431@gmail.com>
2024-03-22 23:47:51 -0400

    mar 22

--- Datasets; Create Day Variable/Create Day Dataset.R
+++ Datasets; Create Day Variable/Create Day Dataset.R
@@ -5,6 +5,7 @@ library(haven)
 library(lubridate)
 df <- read_sav("File To; Jiaqi; 2024; DayIndicator.sav")
 df2 <- read_sav("File To; Jiaqi; 2024; No-String2.sav")
+df3 <- read_sav("File To; Jiaqi; 2024; No-String3.sav")
 df <- df |>
   mutate(Temporary_Date = dmy(SurveySubmittedDate)) |>
   group_by(ID) |>
@@ -17,6 +18,12 @@ df2 <- df2 |>
   arrange(ID, Temporary_Date) |>
   mutate(Wave_DayIndicator = as.integer(Temporary_Date - first(Temporary_Date) + 1))
 
+df3 <- df3 |>
+  group_by(ID, Wave_Day) |>
+  arrange(ID, Wave_Day, Wave_Time_Submitted) |>
+  mutate(Wave_Moment = row_number())
+
 write_sav(df, "File Back To; Marco; 2024; DayIndicator.sav")
 write_sav(df2, "File Back To; Marco; 2024; No-String2.sav")
+write_sav(df3, "File Back To; Marco; 2024; No-String3.sav")
 check <- read_sav("File Back To; Marco; 2024; DayIndicator.sav")
