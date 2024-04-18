c7ff7dddef31308cb08fef9302925d783d0b79bd
Jiaqi-Bi-Not-PhD <77589547+Jiaqi-Bi-Not-PhD@users.noreply.github.com>
2024-04-17 23:06:10 -0400

    apr 17

--- Alice; Data/Alice; Data Merge.R
+++ Alice; Data/Alice; Data Merge.R
@@ -6,23 +6,33 @@ library(lme4)
 library(lmerTest)
 library(readxl)
 
-data_alice <- read_sav("Dataset; Bruneau; EMA.sav")
+data_alice <- read_sav("Dataset; Bruneau; Central.sav")
 data_cortisol <- read_excel("Dataset; Bruneau; Cortisol copy.xlsx")
 data_cortisol <- data_cortisol[,-c(8:18)]
 data_cortisol$Mean <- as.numeric(data_cortisol$Mean)
+data_cortisol$Submission_Code <- 1
+
+#data_alice2 <- subset(data_alice2, Submission_Code == 1)
 
 
 ## Create additional variables in data_alice in order to make the merge easier
 data_merged <- full_join(data_cortisol, data_alice, by = c("ID" = "ID", 
-                                                            "Day_number" = "Wave_Lev2_DayNumber", 
-                                                            "Sample_number" = "Wave_Lev1_MomentNumber"))
+                                                            "Day_number" = "Saliva_Day", 
+                                                            "Sample_number" = "Saliva_Moment", 
+                                                            "Submission_Code" = "Diary_Submission_Code"))
 
-data_merged <- data_merged |> mutate(CV_Percent = `CV%`,
-                                     Wave_Lev2_DayNumber = Day_number, 
-                                     Wave_Lev1_MomentNumber = Sample_number) |> 
+## Check everything
+check <- data_merged |>
+  select(c(Name, Mean, `CV%`, ID, Day_number, Sample_number, 
+           Submission_Code, Diary_Day, Diary_Moment, Diary_Submission_Raw))
+check <- check |>
+  filter(ID == "MGH_EMA_009")
+
+data_merged <- data_merged |> mutate(CV_Percent = `CV%`) |> 
   select(-`CV%`)
+
 check <- data_merged |>
   select(c(Name, ID, Day_number, Sample_number, 
            Mean, Std_Dev, CV_Percent, Raw_Lev1_FU_response_type))
 
-write_sav(data_merged, "Merged Dataset; Bruneau.sav")
+write_sav(data_merged, "Merged Dataset2; Bruneau.sav")
